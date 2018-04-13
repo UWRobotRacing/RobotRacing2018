29acb58bb54ff143fa1762f4b5ba697a011ce1ce
Angela Gu <angu9001@gmail.com>
2018-04-12 21:33:03 -0400

    Update notes.c

--- notes.c
+++ notes.c
@@ -3,29 +3,63 @@
 #include <sensor_msgs/image_encodings.h>
 
 using namespace cv;
-static volatile state;
+static volatile int state;
 
-EndlineCounter::EndlineCounter(ros::NodeHandle nh) : it(nh)
-{
+enum states {
+	BEGINNING = 0x00,
+	START_LINE = 0x03,
+	MIDDLE = 0x04,
+	END_LINE = 0x07,
+	PAST_END = 0x08
+};
+
+Class EndlineCounter {
+	private :
+		ros::NodeHandle nh;
+		
+		image_transport::ImageTransport it;
+		image_transport::Subscriber sub;
+		
+		cv_bridge::CvImagePtr imgptr;
+		cv::Mat img;
+		
+		ros::Publisher pub;
 	
-	nh.advertise<std_msgs::Int8>(endline,1);
-	state = BEGINNING;
+	
+	public :
+		int state;
+		
+		EndlineCounter (ros::NodeHandle nh) : it(nh) 
+		{
+			sub = it.subscribe("tl_camera/image",1, &EndlineCounter::img_callback);
+			nh.advertise<std_msgs::Bool>("endline",1);
+			state = BEGINNING;
+		}
+		
+		int get_state() {
+			return state;
+		}
+		
+		
 }
 
 EndlineCounter::img_callback(const sensor_msgs::ImagePtr& msg) {
-	image_transport::Publisher pub;
-	cv_bridge::CvImagePtr first_img = cv_bridge::toCvCopy(msg, "bgr8")->image); 
+	pub = nh.advertise<std_msgs::Bool>("endline", 1);
+	imgptr = cv_bridge::toCvCopy(msg, "bgr8")->image); 
 	
 	//process image
 	
 	
-	
-	
-	
-	
-	//
-	
 	//publish 
+	if(state & ENDLINE_PAST) {
+		pub.publish(true);
+	} else {
+		pub.publish(false);
+	}
+}
+
+void EndlineCounter::find_el_state() {
+	
 }
 
 
@@ -33,14 +67,9 @@ int main(int argc, char** argv)
 {
 	
 	ros::init(argc,argv, "endline_counter");
-	ros::NodeHandle nh;
 	std::cout << "Initializing endline_counter" << endl;
 	
 	//nodehandler.param ?
-	
-	image_transport::ImageTransport it;
-	image_transport::Subscriber sub = it.subscribe("tl_camera/image",1, &EndlineCounter::img_callback);
-
 	
 	
 	while(ros::ok)
