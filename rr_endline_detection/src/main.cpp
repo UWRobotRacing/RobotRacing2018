2a407e737e13b0cd7de84db9ac5f4a686d6a865b
Angela Gu <angu9001@gmail.com>
2018-04-18 20:38:37 -0400

    Initial project set-up

---
+++ rr_endline_detection/src/main.cpp
@@ -0,0 +1,12 @@
+// Copyright [2018] University of Waterloo Robotics Team
+// Robot Racing Endline Detection Main
+// Author: Angela Gu
+// Date: 2018 04 18
+#include <ros/ros.h>
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+
+}
