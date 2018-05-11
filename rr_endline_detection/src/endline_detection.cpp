91c374e11210995c451e2deedce8ed31457cee4c
Angela Gu <angu9001@gmail.com>
2018-05-10 20:30:27 -0400

    added image conversion code

--- rr_endline_detection/src/endline_detection.cpp
+++ rr_endline_detection/src/endline_detection.cpp
@@ -8,26 +8,34 @@ EndlineCounter::EndlineCounter (ros::NodeHandle nh) : it(nh)
 
 void EndlineCounter::img_callback(const sensor_msgs::ImageConstPtr& msg)
 {
-  ROS_INFO("hello world!");
+	ROS_INFO("hello world!");
+	cv::Mat init_img, hsv_img, mag_img, mag_img2;
 	cv_bridge::CvImagePtr cv_ptr;
+ 	int iLowH = 145;
+ 	int iHighH = 165;
 
 	try
 	{
 		cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+
+		//filter for magenta
+		init_img = cv_ptr->image;
+		cv::cvtColor(init_img,hsv_img, CV_BGR2HSV);
+		cv::inRange(hsv_img, cv::Scalar(iLowH, 0,0), cv::Scalar(iHighH,255,255), mag_img);
+		
+		mag_img2 = mag_img.clone();
+		cv::GaussianBlur(mag_img, mag_img2, cv::Size(7,7), 0, 0);
+
+
 	}
 	catch (cv_bridge::Exception& e)
 	{
 		ROS_ERROR("cv_bridge exception: %s", e.what());
 		return;
 	}
-
-	// Draw an example circle on the video stream
-	if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-	{
-		cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
-	}	
+	
 	// Update GUI Window
-	cv::imshow("OPENCV_WINDOW", cv_ptr->image);
+	cv::imshow("OPENCV_WINDOW", mag_img2);
 	cv::waitKey(3);
 
 	//publish 
