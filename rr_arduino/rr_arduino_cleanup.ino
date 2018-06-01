28d64e3f48ad5f8983879dbfd435b0c39fe26f90
Kiba <kibazohb@mcmaster.ca>
2018-06-01 19:54:46 -0400

    update arduino code

--- rr_arduino/rr_arduino_cleanup.ino
+++ rr_arduino/rr_arduino_cleanup.ino
@@ -1,14 +1,17 @@
-/*********************************************************************************/
-//  Robot Racer Controller                                                       //
-//  Written By: Tom Meredith, Noah Abradjian, Toni Ogunmade, Abhi Srikantharajah //
-//  Last Updated: Feb 26, 2016                                                   //
-/*********************************************************************************/
+/*
+ * @file Robot Racer Controller                                                       
+ * @author Tom Meredith
+ * @author Noah Abradjian 
+ * @author Toni Ogunmade  
+ * @author Abhi Srikantharajah 
+ * @competition IARRC 2018
+ * Last Updated: June 1, 2018                                                   
+ */
 
 //#define TEST_OUTPUT
 
-//!Import Libraries
+//*Import Libraries
 #include <SPI.h>
-#include <grayhill_encoder_LS7266R.h>
 #include <Servo.h>
 #include <robot_racer.h>
 //#define USE_USBCON
@@ -19,54 +22,73 @@
 #include <PID_v1.h>
 
 
-//!Serial defines
+/*
+ *@brief Serial, velocity and battery monitoring defines respectively
+ *
+ */
 #define ROS_BAUD_RATE         57600
 #define EC_BAUD_RATE          115200
 
-//!Velocity encoder defines
-#define ENCODER_FREQUENCY     20
-#define ENCODER_PIN           47
 
-//Battery monitoring defines
+//*I2C address for encoder counter 
+#define SLAVE_ADDRESS 0x07
+
+//*Battery monitoring defines
 #define BATTERY_FREQUENCY     2
 #define BATTERY_PIN           A0
 #define AVERAGING_SIZE        5
 
+/**
+ *@brief function Call
+ * publishes actual velocity & average battery value
+ * returns void
+ */
 void get_battery_state(long current_time);
 
 Car RobotRacer;
 
-int encoder_counts_to_meters = 28673; //!< MUST be a decimal number
+int encoder_counts_to_meters = 28673; // MUST be a decimal number
 Encoder Encoder(ENCODER_PIN, encoder_counts_to_meters, ENCODER_FREQUENCY);
 
-//!PID tuning parameter
-// double throttle_PID_val[3] = {0, 0, 0};
+//PID tuning parameter
+//double throttle_PID_val[3] = {0, 0, 0};
 double rr_velocity = 0.0f , goal_velocity = 0.0f, autonomous_throttle = 1500.0f;
 
-//!PID initialization
+//PID initialization
 // PID ThrottlePID(&rr_velocity, &autonomous_throttle, &goal_velocity,
 //                 throttle_PID_val[0], throttle_PID_val[1], throttle_PID_val[2], DIRECT);
 
-/**********************************************************/
-//               ROS communication setup                  //
-/**********************************************************/
+/*
+ *@brief ROS communication setup begins
+ */
+
+ 
+/*
+ *node initialization
+ */
 ros::NodeHandle nh;
 
+/*
+ *@brief message objects created
+ */
 std_msgs::Float32 cmdVelocity;
 std_msgs::Float32 cmdSteering;
-
 std_msgs::Int8 state_msg;
 std_msgs::Float32 actual_velocity_msg;
 std_msgs::Float32 debug;
 std_msgs::Float32 velDebug;
 std_msgs::Int8 battery_percentage_msg;
 
-//!Prototypes for Callbacks
+/*
+ *@brief Callback functions used by subscriber
+ */
 void cmdVelocityCallback(const std_msgs::Float32 & cmd_vel_msg);
 void cmdSteeringCallback(const std_msgs::Float32 & cmd_str_msg);
 void ThrottlePIDArrayCallback(const std_msgs::Float32MultiArray & array);
 
-//!ROS publisher and subscriber commands
+/*
+ *@brief ROS publisher and subscriber commands
+ */
 ros::Publisher state("/arduino/vehicle_state", &state_msg);
 ros::Publisher encoder("/arduino/enc_vel", &actual_velocity_msg);
 ros::Publisher debugger ("/arduino/debug", &debug);
@@ -79,14 +101,22 @@ ros::Subscriber <std_msgs::Float32MultiArray> PID_msg ("/Throttle_PID_array", Th
 int steeringAngle = 1500;
 int ROS_watchdog = 0;
 
-
+/**
+ *@brief general setup for node handler & robot racer
+ *sets the data rate for serial transmition
+ *@param data rate per second at serial port
+ *returns void
+ */
 void setup() {
 #ifdef TEST_OUTPUT
   Serial.begin(ROS_BAUD_RATE);
 #endif
   Serial2.begin(EC_BAUD_RATE);
-  
-  //!ROS Node Handler setup
+
+/*
+ *ROS Node Handler setup
+ *@param specified topics
+ */
   nh.getHardware()->setBaud(ROS_BAUD_RATE);
   nh.initNode();
   nh.advertise(state);
@@ -99,16 +129,20 @@ void setup() {
   Encoder.setup();
 
   RobotRacer.setup();
-  //!PID setup
-  // ThrottlePID.SetMode(AUTOMATIC); //!<turn the PID on
-  // ThrottlePID.SetOutputLimits(NEUTRAL, THROTTLE_MAX); //!<sets range for throttle PID contoller
-  // ThrottlePID.SetSampleTime(20); //!<PID algorithm evaluates every 50ms
-  // ThrottlePID.SetTunings(333, 0,0.0);
+/**
+ *PID setup
+ * ThrottlePID.SetMode(AUTOMATIC); //!<turn the PID on
+ *ThrottlePID.SetOutputLimits(NEUTRAL, THROTTLE_MAX); //!<sets range for throttle PID contoller
+ *ThrottlePID.SetSampleTime(20); //!<PID algorithm evaluates every 50ms
+ *ThrottlePID.SetTunings(333, 0,0.0);
+ */
   pinMode(13,OUTPUT);
 }
 
+/**
+ *@brief we use RC values and car state to publish actual_velocity and state_msg
+ */
 long previous_time = 0; //!<for update encoder speed loop
-
 void loop() {
   RobotRacer.RC_read(); //!<get RC controller values
   nh.spinOnce();
@@ -121,37 +155,50 @@ void loop() {
     encoder.publish(&actual_velocity_msg);
   }
   switch (RobotRacer.GetState()) {
-    case ESTOP: //!<case for emergency stop
+/*
+ *case for emergency stop
+ */
+    case ESTOP:
       RobotRacer.Estop();
       break;
-
-    case RC: //!<case for RC control
+/*
+ *case for RC control
+ */
+    case RC:
       RobotRacer.RCMode();
       break;
 
-    case AUTO: //!<case for autonomous mode
+
+/*
+ *case for autonomous mode
+ */
+    case AUTO:
       // ThrottlePID.Compute();
       RobotRacer.SetThrottle((int)autonomous_throttle);
-      //!maps angle from -30 to 30 deg to MIN_STEER_VAL to MAX_STEER_VAL
+      //maps angle from -30 to 30 deg to MIN_STEER_VAL to MAX_STEER_VAL
       RobotRacer.SetSteering((int)steeringAngle);
-   
+
       break;
   }
   state_msg.data = RobotRacer.GetState();
   state.publish(&state_msg);
-  
+
   get_battery_state(current_time);
 }
-
+/*
+*@brief publishes battery percentage at a specified BATTERY_FREQUENCY
+*@param current time
+*returns void
+*/
 void get_battery_state(long current_time){
   static long prev_time = 0;
-  //publish the battery percentage at specified frequency
   if ((current_time - prev_time) * BATTERY_FREQUENCY >= 1000){
-    //Get and calculate battery percentage
     long battery_value = analogRead(BATTERY_PIN);
     int battery_percentage = 100 * battery_value / 1024;
 
-    //Set up array for low pass averaging at first time
+/**
+ *@brief Set up array for low pass averaging at first time
+ */
     static int percentages[AVERAGING_SIZE];
     if (prev_time == 0){
       for (int i = 0; i < AVERAGING_SIZE; i++){
@@ -159,7 +206,9 @@ void get_battery_state(long current_time){
       }
     }
 
-    //update array, calculate sum
+/*
+ *@brief update array, calculate sum
+ */
     int sum = 0;
     for (int i = AVERAGING_SIZE - 1; i > 0; --i){
       percentages[i] = percentages[i - 1];
@@ -168,7 +217,9 @@ void get_battery_state(long current_time){
     percentages[0] = battery_percentage;
     sum += battery_percentage;
 
-    //publish the average value
+/*
+ *publish the average value
+ */
     battery_percentage_msg.data = (int)(sum / AVERAGING_SIZE);
     battery.publish(&battery_percentage_msg);
 
