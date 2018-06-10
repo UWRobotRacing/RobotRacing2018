9243d7c229fee9f14c11b01a9dd94f41dfade61e
Oluwatoni <ooogunma@edu.uwaterloo.ca>
2018-06-09 20:02:43 -0700

    fix the hysteresis logic

--- rr_endline_detection/include/endline_detection.hpp
+++ rr_endline_detection/include/endline_detection.hpp
@@ -4,28 +4,24 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
-#include <std_msgs/Bool.h>
 #include <sensor_msgs/image_encodings.h>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
 #include <opencv2/opencv.hpp>
 #include <std_srvs/Trigger.h>
 
 class EndlineCounter {
-	private :
-		ros::NodeHandle nh_;
-		image_transport::ImageTransport it_;
-		
-		cv_bridge::CvImagePtr imgptr;
-		cv::Mat img;
-		
-		ros::ServiceClient client_;
-	
-	public :
-		uint8_t status;
-		EndlineCounter(ros::NodeHandle);
-		void img_callback(const sensor_msgs::ImageConstPtr&);
-		bool blob_detector(cv::Mat);
+  private :
+    ros::NodeHandle nh_;
+    image_transport::ImageTransport it_;
+    
+    ros::ServiceClient client_;
+    bool detection_status_;
+    int hysteresis_counter_;
+    int hysteresis_constant_;
+  
+  public :
+    EndlineCounter(ros::NodeHandle);
+    void img_callback(const sensor_msgs::ImageConstPtr&);
+    bool blob_detector(cv::Mat);
 };
 
 #endif /*ENDLINE_DETECTION_HPP*/
--- rr_endline_detection/src/endline_detection.cpp
+++ rr_endline_detection/src/endline_detection.cpp
@@ -3,78 +3,107 @@
 //constructor
 EndlineCounter::EndlineCounter (ros::NodeHandle nh_) : it_(nh_) 
 {
-	//pub_ = nh_.advertise<std_msgs::Bool>("endline",1);
-	status = 0;
-	client_ = nh_.serviceClient<std_srvs::Trigger>("/Supervisor/count_lap", true);
+  detection_status_ = false;
+  hysteresis_counter_ = 0;
+  hysteresis_constant_ = 2;
+  client_ = nh_.serviceClient<std_srvs::Trigger>("/Supervisor/count_lap", true);
 }
 
 //callback to handle detection
 void EndlineCounter::img_callback(const sensor_msgs::ImageConstPtr& msg)
 {
-	cv::Mat init_img, hsv_img, mag_img, blur_img;
-	cv_bridge::CvImagePtr cv_ptr;
- 	int iLowH = 145;
- 	int iHighH = 165;
-	std_srvs::Trigger srv;
+  cv::Mat init_img, hsv_img, mag_img, blur_img;
+  cv_bridge::CvImagePtr cv_ptr;
+  int iLowH = 145;
+  int iHighH = 165;
+  std_srvs::Trigger srv;
 
-	try
-	{
-		cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+  try
+  {
+    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
 
-		//filter for magenta
-		init_img = cv_ptr->image;
-		cv::cvtColor(init_img, hsv_img, CV_BGR2HSV);
-		cv::inRange(hsv_img, cv::Scalar(iLowH, 0,0), cv::Scalar(iHighH,255,255), mag_img);
-		
-		blur_img = mag_img.clone();
-		cv::GaussianBlur(mag_img, blur_img, cv::Size(7,7), 0, 0);
+    //filter for magenta
+    init_img = cv_ptr->image;
+    cv::cvtColor(init_img, hsv_img, CV_BGR2HSV);
+    cv::inRange(hsv_img, cv::Scalar(iLowH, 0,0), cv::Scalar(iHighH,255,255), mag_img);
+    
+    cv::GaussianBlur(mag_img, mag_img, cv::Size(7,7), 0, 0);
 
-		//detect endline
-		status = status << 1;
-		if (blob_detector(blur_img)){
-			++status;
-			ROS_INFO("sts %x", status);
-		}
-	}
-	catch (cv_bridge::Exception& e)
-	{
-		ROS_ERROR("cv_bridge exception: %s", e.what());
-		return;
-	}
-	
-	// Update GUI Window
-	cv::imshow("OPENCV_WINDOW", blur_img);
-	cv::waitKey(3);
-
-	//notify service if consecutive 1's
-	status &= 0xF;
-	ROS_INFO("2sts %x", status);
-	if (status > 8) {
-		if(client_.call(srv))
-		{
-			if (srv.response.success){
-				ROS_INFO("SUCCESS");
-			}
-			ROS_INFO("TRUE");
-		} else {
-			ROS_INFO("FALSE");
-		}
-	}
+    //detect endline
+    if (blob_detector(mag_img))
+    {
+      if (!detection_status_)
+      {
+        hysteresis_counter_++;
+        if (hysteresis_counter_ > hysteresis_constant_)
+        {
+          hysteresis_counter_ = 0;
+          detection_status_ = true;
+          ROS_INFO("DETECTED");
+          if(client_.call(srv))
+          {
+            if (srv.response.success)
+            {
+              ROS_INFO("SUCCESS");
+            }
+          }
+        }
+      }
+      else
+      {
+        if (hysteresis_counter_)
+        {
+          hysteresis_counter_--;
+        }
+      }
+    }
+    else
+    {
+      if (!detection_status_)
+      {
+        if (hysteresis_counter_)
+        {
+          hysteresis_counter_--;
+        }
+      }
+      else
+      {
+        hysteresis_counter_++;
+        if (hysteresis_counter_ > hysteresis_constant_){
+          hysteresis_counter_ = 0;
+          detection_status_ = false;
+          ROS_INFO("NO LONGER DETECTED");
+        }
+      }
+    }
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
+    return;
+  }
 }
 
 //determines by area if blob is large enough
- bool EndlineCounter::blob_detector(cv::Mat img) {
-	cv::SimpleBlobDetector::Params params;
-	params.filterByArea = true;
-	params.minArea = 50;
+bool EndlineCounter::blob_detector(cv::Mat img)
+{
+  cv::SimpleBlobDetector::Params params;
+  params.filterByArea = true;
+  params.filterByCircularity = true;
+  params.filterByColor = false;
+  params.filterByInertia = false;
+  params.filterByConvexity = false;
+  params.minArea = 250;
+  params.maxCircularity = 0.4;
+  params.minCircularity = 0;
 
-	cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);
-	std::vector<cv::KeyPoint> keypoints;
-	detector->detect(img, keypoints);
+  cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);
+  std::vector<cv::KeyPoint> keypoints;
+  detector->detect(img, keypoints);
 
-	if (keypoints.size() > 0) {
- 		return true;
-	}
-	
-	return false;
- }
+  if (keypoints.size() > 0)
+  {
+    return true;
+  }
+  return false;
+}
