42f8d6c79c8d4552afd3870381355203c1ed8e71
Waleed <w29ahmed@edu.uwaterloo.ca>
2018-06-19 15:40:41 -0400

    Implemented race metrics logging

--- rr_supervisor/include/supervisor.hpp
+++ rr_supervisor/include/supervisor.hpp
@@ -12,22 +12,27 @@
 
 #include <ros/ros.h>
 #include <std_msgs/Bool.h>
+#include <geometry_msgs/Twist.h>
 #include <std_srvs/Trigger.h>
 #include <std_srvs/Empty.h>
+
 
 class Supervisor
 {
   public:
     Supervisor();
-    // Service callback methods
+    // Callback methods
     bool startRace(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res);
     bool countLap(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res);
+    void trackSpeed(const geometry_msgs::TwistConstPtr& msg);
+    void finishRace();
   private:
     // ROS Variables
     ros::NodeHandle nh_;
     ros::Publisher twist_pub;
     ros::Publisher null_lock;
     ros::Publisher remove_null_lock;
+    ros::Subscriber cmd_sub;
     ros::ServiceServer start_race_service;
     ros::ServiceServer count_lap_service;
 
@@ -35,6 +40,11 @@ class Supervisor
     std_msgs::Bool bool_msg;
     std::string race_type;
     int lap_count;
+    int twist_msg_count;
+    float speed_sum;
+    clock_t begin_time;
+    float race_time;
+    float average_speed;
 };
 
 #endif  // SUPERVISOR_H
--- rr_supervisor/src/supervisor.cpp
+++ rr_supervisor/src/supervisor.cpp
@@ -4,20 +4,28 @@
 // Date: 2018 05 25
 
 #include <supervisor.hpp>
-#include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Vector3.h>
+#include <math.h>
+#include <time.h>
+#include <fstream>
+#include <sstream>
 
 // Supervisor Class constructor
 Supervisor::Supervisor()
 {
     // Initialize variables
     lap_count = 0;
+    speed_sum = 0.0;
+    twist_msg_count = 0;
     nh_.param<std::string>("Launch/race_type", race_type, "drag");
 
     // Setup publishers for twist multiplexer
     twist_pub = nh_.advertise<geometry_msgs::Twist>("/Supervisor/twist_mux", 1);
     null_lock = nh_.advertise<std_msgs::Bool>("/Supervisor/no_movement", 1);
     remove_null_lock = nh_.advertise<std_msgs::Bool>("/Supervisor/enable_movement", 1);
+
+    // Subscribe to vehicle's velocity publisher to calculate average speed
+    cmd_sub = nh_.subscribe("/rr_vehicle/vel_cmd", 1, &Supervisor::trackSpeed, this);
 
     // Setup service servers
     start_race_service = nh_.advertiseService("/Supervisor/start_race", &Supervisor::startRace, this);
@@ -49,6 +57,9 @@ bool Supervisor::startRace(std_srvs::Empty::Request &req, std_srvs::Empty::Respo
     bool_msg.data = true;
     remove_null_lock.publish(bool_msg);
 
+    // Start timer in order to log race time
+    begin_time = clock();
+
     return true;
 }
 
@@ -73,5 +84,57 @@ bool Supervisor::countLap(std_srvs::Trigger::Request &req, std_srvs::Trigger::Re
             }
         }
     }
+
+    if (res.success) {
+        finishRace();
+    }
     return true;
+}
+
+/**
+ * @brief subscriber callback method for tracking vehicle speed
+ * @param msg twist message published to vehicle
+ */
+void Supervisor::trackSpeed(const geometry_msgs::TwistConstPtr& msg)
+{
+    twist_msg_count += 1;
+    speed_sum += sqrt(pow(msg->linear.x, 2) + pow(msg->linear.y, 2));
+}
+
+/**
+ * @brief handle end of race events, namely logging race metrics
+ */
+void Supervisor::finishRace()
+{
+    // Calculate average speed and elapsed time since start of race
+    average_speed = speed_sum / twist_msg_count;
+    race_time = float(clock() - begin_time) / CLOCKS_PER_SEC;
+
+    // Construct a timestamp with the following format:
+    // Year_Month_day--Hours_minutes_seconds
+    time_t rawtime;
+    struct tm * time_info;
+    char date_string[50];
+    time(&rawtime);
+    time_info = localtime(&rawtime);
+    strftime(date_string, 50, "%Y_%m_%d--%H_%M_%S", time_info);
+
+    // Open a file to write to
+    std::ofstream race_metrics_file;
+    std::ostringstream file_name;
+    file_name << "~/" << race_type << "_" << date_string << ".txt";
+    race_metrics_file.open(file_name.str().c_str());
+
+    // Write to file
+    std::ostringstream metrics;
+
+    metrics << "Date: " << date_string << "\n";
+    metrics << "Race type: " << race_type << "\n";
+    metrics << "Race time: " << race_time << "\n";
+    metrics << "Average speed: " << average_speed << "\n";
+
+    race_metrics_file << metrics.str();
+    race_metrics_file.close();
+
+    ros::shutdown();
 }
