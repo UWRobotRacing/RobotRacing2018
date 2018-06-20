0997f0a4c768a336637835542ef1a8fd7d2fea17
Oluwatoni <ooogunma@edu.uwaterloo.ca>
2018-06-19 23:48:27 -0700

    cleanup some spaces and tabs

--- rr_endline_detection/src/main.cpp
+++ rr_endline_detection/src/main.cpp
@@ -8,13 +8,13 @@
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "endline_detection");
-	ROS_INFO("Initializing endline_detection");	
-    std::string camera_source = "/rr_vehicle/front_facing_cam/image_raw";
-    ros::NodeHandle nh_;
-	image_transport::ImageTransport it_(nh_);
-    EndlineCounter ec(nh_);
-    image_transport::Subscriber sub_ = it_.subscribe(camera_source, 1, &EndlineCounter::ImgCb, &ec);
-	ros::spin();
-    return 0;
+  ros::init(argc, argv, "endline_detection");
+  ROS_INFO("Initializing endline_detection");	
+  std::string camera_source = "/rr_vehicle/front_facing_cam/image_raw";
+  ros::NodeHandle nh_;
+  image_transport::ImageTransport it_(nh_);
+  EndlineCounter ec(nh_);
+  image_transport::Subscriber sub_ = it_.subscribe(camera_source, 1, &EndlineCounter::ImgCb, &ec);
+  ros::spin();
+  return 0;
 }
