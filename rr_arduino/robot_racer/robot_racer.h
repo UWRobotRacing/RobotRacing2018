2a5c1c56d0a15dd8a4a1174cd0ea56fba5431ac7
Kiba <kibazohb@mcmaster.ca>
2018-06-27 01:46:26 -0400

    fix errors

--- rr_arduino/robot_racer/robot_racer.h
+++ rr_arduino/robot_racer/robot_racer.h
@@ -8,29 +8,29 @@
 #define TEST_OUTPUT 1
 
 // servo defines
-#define SERVO_THROTTLE_PIN    10
-#define SERVO_STEER_PIN       11
-#define SERVO_BRAKE_PIN       12
-#define MANUAL_MAX            1575
-#define MANUAL_REV_MAX        1375
-#define THROTTLE_MAX          1675  //< 1675 is 10 m/s, max. is 2000
-#define THROTTLE_REV_MAX      1300  //< Under 1300 is too fast, min. is 1000
-#define MAX_RC_VAL            1738
-#define MIN_RC_VAL            304
-#define REST_RC_VAL           874
-#define MAX_RC_STEER_VAL      1726
-#define MIN_RC_STEER_VAL      306
-#define REST_STEER_VAL        982
-#define MAX_STEERING          2000
-#define MIN_STEERING          1000
-#define NEUTRAL               1500
-#define STEER_NEUTRAL         1460
-#define MAX_STEERING_ANGLE    0.5236
-#define MIN_STEERING_ANGLE    (-0.5236)
-#define THROTTLE_REDUCTION    25
-#define BRAKE_DELAY           100  //< Reducing this quickens the vehicle braking
-#define RC_CHANNELS           8
-#define DELAY                 250 // Time in milliseconds for estop to trigger when r/c disconnects
+#define SERVO_THROTTLE_PIN = 10
+#define SERVO_STEER_PIN    = 11
+#define SERVO_BRAKE_PIN    = 12
+#define MANUAL_MAX         = 1575
+#define MANUAL_REV_MAX     = 1375
+#define THROTTLE_MAX       = 1675  //< 1675 is 10 m/s, max. is 2000
+#define THROTTLE_REV_MAX   = 1300  //< Under 1300 is too fast, min. is 1000
+#define MAX_RC_VAL         = 1738
+#define MIN_RC_VAL         = 304
+#define REST_RC_VAL        = 874
+#define MAX_RC_STEER_VAL   = 1726
+#define MIN_RC_STEER_VAL   = 306
+#define REST_STEER_VAL     = 982
+#define MAX_STEERING       = 2000
+#define MIN_STEERING       = 1000
+#define NEUTRAL            = 1500
+#define STEER_NEUTRAL      = 1460
+#define MAX_STEERING_ANGLE = 0.5236
+#define MIN_STEERING_ANGLE = (-0.5236)
+#define THROTTLE_REDUCTION = 25
+#define BRAKE_DELAY        = 100  //< Reducing this quickens the vehicle braking
+#define RC_CHANNELS        = 8
+#define DELAY              = 250 // Time in milliseconds for estop to trigger when r/c disconnects
 
 // States for eStop, RC, and Auto
 enum CarState
