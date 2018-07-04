70b338d6b88f9cca1cbb1f312cfd86e6e145f73c
TranBrian10 <tranbrian10@gmail.com>
2018-07-04 12:39:01 -0400

    mean lab values

--- rr_lane_detection/include/shadow_removal.hpp
+++ rr_lane_detection/include/shadow_removal.hpp
@@ -7,6 +7,8 @@
 
 // includes
 #include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 void RemoveShadows(const cv::Mat &input_image, cv::Mat &output_image);
 #endif //__SHADOW_REMOVAL_HPP
--- rr_lane_detection/src/shadow_removal.cpp
+++ rr_lane_detection/src/shadow_removal.cpp
@@ -5,8 +5,6 @@
 
 #include "shadow_removal.hpp"
 
-#include <opencv2/opencv.hpp>
-
 /** @brief This function is intended to detect shadows and reduce the difference
  *         in illumination in those regions
  *
@@ -18,5 +16,21 @@
  *          width and height as the input matrix
  */
 void RemoveShadows(const cv::Mat &input_image, cv::Mat &output_image) {
-	// return input_image;
+	// Configurable constants
+    const double THRESHOLD_TOLERANCE = 0.8;
+    const double MIN_SHADOW_AREA = 30;
+    const double MASK_DILATION = 6;
+    const double EXPANDED_SHADOW_MASK_DILATION = 10;
+    const double EXPANDED_EDGE_MASK_DILATION = 8;
+    const double MED_FILTER_KERNEL_SIZE = 15;
+
+	// Get the mean of L, a, b planes
+	cv::Mat shadow_lab;
+	cvtColor(input_image, shadow_lab, CV_BGR2Lab, 0);
+
+	std::vector<cv::Mat> channels;
+	split(shadow_lab, channels);
+	cv::Scalar mean_l = mean(channels[0]);
+	cv::Scalar mean_a = mean(channels[1]);
+	cv::Scalar mean_b = mean(channels[2]);
 }
