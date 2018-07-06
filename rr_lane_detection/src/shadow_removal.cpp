b3c6aff535dd7202cb676cf10b95d2f41661dc24
TranBrian10 <tranbrian10@gmail.com>
2018-07-06 01:07:52 -0400

    over-illuminated edge correction

--- rr_lane_detection/src/shadow_removal.cpp
+++ rr_lane_detection/src/shadow_removal.cpp
@@ -22,7 +22,7 @@ void RemoveShadows(const cv::Mat &input_image, cv::Mat &output_image) {
     const double MIN_SHADOW_AREA = 30;
     const double MASK_DILATION = 6;
     const double EXPANDED_SHADOW_MASK_DILATION = 10;
-    const double EXPANDED_EDGE_MASK_DILATION = 8;
+    const double CORRECTION_MASK_EROSION = 4;
     const double MED_FILTER_KERNEL_SIZE = 15;
 
 	// Get the mean of L, a, b planes
@@ -76,39 +76,60 @@ void RemoveShadows(const cv::Mat &input_image, cv::Mat &output_image) {
 	int num_components = cv::connectedComponents(shadow_mask_morph, connected_components, 4);
 
 	// Shadow removal
-	cv::Mat corrected_image = input_image.clone();
+	cv::Mat shadow_removed_image = input_image.clone();
 
 	for (int i = 1; i <= num_components; i++) {
 		cv::Mat component_mask;
 		cv::inRange(connected_components, cv::Scalar(i), cv::Scalar(i), component_mask);
 
-		cv::Mat dilated_component_mask;
+		cv::Mat dilated_component_mask, dilated_component_edge;
 		kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(EXPANDED_SHADOW_MASK_DILATION, EXPANDED_SHADOW_MASK_DILATION));
 		cv::dilate(component_mask, dilated_component_mask, kernel);
-		cv::bitwise_not(dilated_component_mask, component_mask, dilated_component_mask);
+		cv::bitwise_not(dilated_component_mask, dilated_component_edge, component_mask);
 
-		// Get mean RGB in masks
-		cv::Scalar outer_mean_rgb = mean(corrected_image, dilated_component_mask);
-		cv::Scalar inner_mean_rgb = mean(corrected_image, component_mask);
+		// Get mean BGR in masks
+		cv::Scalar outer_mean_rgb = mean(shadow_removed_image, dilated_component_edge);
+		cv::Scalar inner_mean_rgb = mean(shadow_removed_image, component_mask);
 
-		cv::Scalar rgb_ratios(outer_mean_rgb[0] / inner_mean_rgb[0],
+		cv::Scalar bgr_ratios(outer_mean_rgb[0] / inner_mean_rgb[0],
 							outer_mean_rgb[1] / inner_mean_rgb[1],
 							outer_mean_rgb[2] / inner_mean_rgb[2]);
 
-		if (std::isinf(rgb_ratios[0])) {
-			rgb_ratios[0] = 1;
+		if (std::isinf(bgr_ratios[0])) {
+			bgr_ratios[0] = 1;
 		}
-		if (std::isinf(rgb_ratios[1])) {
-			rgb_ratios[1] = 1;
+		if (std::isinf(bgr_ratios[1])) {
+			bgr_ratios[1] = 1;
 		}
-		if (std::isinf(rgb_ratios[2])) {
-			rgb_ratios[2] = 1;
+		if (std::isinf(bgr_ratios[2])) {
+			bgr_ratios[2] = 1;
 		}
 
 		// Multiply shadow region by ratios to remove shadow
 		cv::Mat component_region;
-		corrected_image.copyTo(component_region, component_mask);
-		component_region = component_region.mul(rgb_ratios);
-		component_region.copyTo(corrected_image, component_mask);
+		shadow_removed_image.copyTo(component_region, component_mask);
+		component_region = component_region.mul(bgr_ratios);
+		component_region.copyTo(shadow_removed_image, component_mask);
 	}
+
+	// Fix over-illuminated edges with median filter
+	cv::Mat corrected_image;
+	cvtColor(shadow_removed_image, corrected_image, CV_BGR2HSV, 0);
+
+	for (int i = 1; i <= num_components; i++) {
+		cv::Mat component_mask;
+		cv::inRange(connected_components, cv::Scalar(i), cv::Scalar(i), component_mask);
+
+		cv::Mat eroded_component_mask, component_edge;
+		kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(CORRECTION_MASK_EROSION, CORRECTION_MASK_EROSION));
+		cv::erode(component_mask, eroded_component_mask, kernel);
+		cv::bitwise_not(component_mask, component_edge, eroded_component_mask);
+
+		cv::Mat median_filtered_image;
+		cv::medianBlur(corrected_image, median_filtered_image, MED_FILTER_KERNEL_SIZE);
+
+		median_filtered_image.copyTo(corrected_image, component_edge);
+	}
+
+	cvtColor(corrected_image, output_image, CV_HSV2BGR, 0);
 }
