89e985c456f06a3aaf1065c02b3a70ff02dd1703
Andrew Jin <adriankitmalaran@gmail.com>
2018-07-09 22:51:43 -0400

    change the robot_race file

--- rr_arduino/robot_racer/robot_racer.h
+++ rr_arduino/robot_racer/robot_racer.h
@@ -4,35 +4,35 @@
 #include "Servo.h"
 #include "Arduino.h"
 
-// #define BRAKE
+//! #define BRAKE
 #define TEST_OUTPUT 1
 
-// servo defines
-const int   SERVO_THROTTLE_PIN = 9;
-const int   SERVO_STEER_PIN    = 11;
-const int   SERVO_BRAKE_PIN    = 12;
-const int   MANUAL_MAX         = 1575;
-const int   MANUAL_REV_MAX     = 1375;
-const int   THROTTLE_MAX       = 1675;  //< 1675 is 10 m/s, max. is 2000
-const int   THROTTLE_REV_MAX   = 1300;  //< Under 1300 is too fast, min. is 1000
-const int   MAX_RC_VAL         = 1738;
-const int   MIN_RC_VAL         = 304;
-const int   REST_RC_VAL        = 874;
-const int   MAX_RC_STEER_VAL   = 1726;
-const int   MIN_RC_STEER_VAL   = 306;
-const int   REST_STEER_VAL     = 982;
-const int   MAX_STEERING       = 2000;
-const int   MIN_STEERING       = 1000;
-const int   NEUTRAL            = 1500;
-const int   STEER_NEUTRAL      = 1460;
-const float MAX_STEERING_ANGLE = 0.5236;
-const float MIN_STEERING_ANGLE = (-0.5236);
-const int   THROTTLE_REDUCTION = 25;
-const int   BRAKE_DELAY        = 100;  //< Reducing this quickens the vehicle   braking
-const int   RC_CHANNELS        = 8;
-const int   DELAY              = 750; // Time in milliseconds for estop to trigger when r/c disconnect
+//! servo defines
+#define SERVO_THROTTLE_PIN    9
+#define SERVO_STEER_PIN       11
+#define SERVO_BRAKE_PIN       12
+#define MANUAL_MAX            1575
+#define MANUAL_REV_MAX        1375
+#define THROTTLE_MAX          1675  //!< 1675 is 10 m/s, max. is 2000
+#define THROTTLE_REV_MAX      1300  //!< Under 1300 is too fast, min. is 1000
+#define MAX_RC_VAL            1738
+#define MIN_RC_VAL            304
+#define REST_RC_VAL           874
+#define MAX_RC_STEER_VAL      1726
+#define MIN_RC_STEER_VAL      306
+#define REST_STEER_VAL        982
+#define MAX_STEERING          2000
+#define MIN_STEERING          1000
+#define NEUTRAL               1500
+#define STEER_NEUTRAL         1460
+#define MAX_STEERING_ANGLE    0.5236
+#define MIN_STEERING_ANGLE    (-0.5236)
+#define THROTTLE_REDUCTION    25
+#define BRAKE_DELAY           100  //!< Reducing this quickens the vehicle braking
+#define RC_CHANNELS           8
+#define DELAY                 250 // Time in milliseconds for estop to trigger when r/c disconnects
 
-// States for eStop, RC, and Auto
+//! States for eStop, RC, and Auto
 enum CarState
 {
   ESTOP,
@@ -40,7 +40,7 @@ enum CarState
   AUTO
 };
 
-// Has all variables for the car
+//! Has all variables for the car
 class Car
 {
   private:
@@ -62,7 +62,7 @@ class Car
     
     Servo ThrottleServo_, SteerServo_;
 #ifdef BRAKE
-    Servo BrakeServo_;//< brakeServo not used?
+    Servo BrakeServo_;//!< brakeServo not used?
 #endif
 
   public:
@@ -73,7 +73,8 @@ class Car
     void SetSteering(int newSteering);
     void SetState(CarState set);
     void Estop();
-    void RC_read();
+    void CheckController();
+    void RC_read(int *incoming_bytes);
     void RCMode();
     void WriteToServos();
     long GetPreviousTime();
